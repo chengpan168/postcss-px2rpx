7a2488dae316aac541b242972497be579546e635
panwang.chengpw <chengpan168@126.com>
2017-12-15 17:12:26 +0800

    opt default

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const postcss = require('postcss')
 
-module.exports = postcss.plugin('wx-px2rpx', (opts) => {
+module.exports = postcss.plugin('wx-px2rpx', (opts = {}) => {
   const { proportion = 1 } = opts
 
   const pxRegExp = /\b(\d+(\.\d+)?)px\b/
